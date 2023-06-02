4617f9e20e260db5c5738a0a00f6f50618facfed
Artem-Podoliak <podoliak.a.s@gmail.com>
2023-06-02 08:01:19 +0300

    new update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
-import contactListTest from '../data/contactListTest.json';
+// import contactListTest from '../data/contactListTest.json';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
